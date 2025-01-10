bb03d1fca24bd9e454eb31578fc1053655155fdc
Pedro vinicius V7 <pedrn770@gmail.com>
2025-01-10 00:25:27 -0300

    refatorar configuração de ambiente e atualizar lógica de estoque

--- frontend/src/assets/js/estoque.js
+++ frontend/src/assets/js/estoque.js
@@ -6,7 +6,7 @@ document.getElementById('form-estoque').addEventListener('submit' , async functi
     const validade = document.getElementById('validade').value;
     const Quantidade = document.getElementById('Quantidade').value;
 
-    const response = await fetch('http://localhost:3000/clientes', {
+    const response = await fetch('http://localhost:3000/estoque', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -17,10 +17,32 @@ document.getElementById('form-estoque').addEventListener('submit' , async functi
     if (response.ok) {
         alert('Cliente adicionado com sucesso!');
         document.getElementById('form-clientes').reset();
-        carregarClientes(); // Atualiza a lista de clientes
+        carregarProdutos(); // Atualiza a lista de 
     } else {
         alert('Erro ao adicionar cliente.');
     }
 
 });
 
+async function carregarProdutos() {
+    const response = await fetch('http://localhost:3000/estoque');
+    const produtos = await response.json();
+
+    const container = document.querySelector('.tabela-de-estoque');
+    container.innerHTML = ''; // Limpa a lista anterior
+
+    produtos.forEach(produtos => {
+        const produtosDiv = document.createElement('div');
+        produtosDiv.innerHTML = `
+            <h1>${produtos.nome}</h1>
+            <p>CPF: ${produtos.cpf}</p>
+            <p>Endereço: ${produtos.endereco}</p>
+            <p>Data de Nascimento: ${produtos.nascimento}</p>
+            <p>Cidade: ${produtos.cidade}</p>
+        `;
+        container.appendChild(produtosDiv);
+    });
+}
+
+// Carrega os clientes ao carregar a página
+window.onload = carregarProdutos;
